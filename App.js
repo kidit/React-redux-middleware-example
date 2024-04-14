97b13ca43c4cee3f3d391a08acdbaaa8afe0e902
kiseno <116077544+kidit@users.noreply.github.com>
2024-04-14 19:54:25 +0900

    latest code

---
+++ App.js
@@ -0,0 +1,11 @@
+import CounterContainer from "./containers/CounterContainer";
+import SampleContainer from "./containers/SampleContainer";
+const App = () => {
+  return(
+      <div>
+        <CounterContainer/>
+      </div>
+  );
+};
+
+export default App;
---
+++ index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {applyMiddleware, createStore} from 'redux';
+import rootReducer, {rootSaga} from './modules';
+import {Provider} from 'react-redux';
+import {createLogger} from 'redux-logger';
+import {thunk} from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+import {composeWithDevTools} from 'redux-devtools-extension';
+
+const logger = createLogger();
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(logger, thunk, sagaMiddleware))
+);
+
+sagaMiddleware.run(rootSaga);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+);
